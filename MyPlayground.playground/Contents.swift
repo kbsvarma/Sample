e085bcab6f8c2f9e4884b3069e2f5c430b901631
Varma Kammili <kbsvarma@gmail.com>
2015-08-15 18:22:19 -0500

    Practice

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground";
+
+
+
+
+
+
+
+
+
+
--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enteredValue: UITextField!
+    @IBOutlet weak var newEnter: UITextField!
+    
+    @IBOutlet weak var display: UILabel!
+    @IBAction func submitButton(sender: AnyObject) {
+        
+        // display.text = "Button Clicked"
+        
+        var modAge = Int(newEnter.text!)!
+        modAge = modAge + 13
+        display.text = "The new age is \(modAge)"
+
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
